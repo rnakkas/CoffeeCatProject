892c75f6855dbfba47981f10fc656919d97e14c0
rnakkas <rahman.nakkas@gmail.com>
2024-10-14 10:46:56 +1100

    added score hud
    added score hud to top left of screen

---
+++ Scripts/hud.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+@onready var coffee_score: Node = $coffee_score;
+@onready var level_manager: Node = %level_manager;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	coffee_score.text = str(level_manager.coffee_score);
