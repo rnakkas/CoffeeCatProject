a580f3caae768b8a99ba36495e3978377b058f90
rnakkas <rahman.nakkas@gmail.com>
2024-10-14 13:14:55 +1100

    updated tween

--- Scripts/ItemCollection.gd
+++ Scripts/ItemCollection.gd
@@ -7,9 +7,9 @@ func player_collects_item(object: Object, animation: AnimatedSprite2D, level_man
 	
 	var tween_1: Tween = object.get_tree().create_tween();
 	var tween_2: Tween = object.get_tree().create_tween();
-	animation.play("collect");
-	tween_1.tween_property(object, "modulate:a", 0, 1.6);
-	tween_2.tween_property(object, "position", object.position - Vector2(0, 210), 0.45);
+	#animation.play("collect");
+	tween_1.tween_property(object, "modulate:a", 0, 0.4);
+	tween_2.tween_property(object, "position", object.position - Vector2(0, 150), 0.5);
 	tween_2.tween_callback(object.queue_free);
 
 func _increase_score(object: Object, level_manager: Node) -> void:
