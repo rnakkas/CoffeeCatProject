b8eff770fe383055d79cd2047cb44ddb2df65793
rnakkas <rahman.nakkas@gmail.com>
2024-11-28 22:13:41 +1100

    improved shooting component
    - improved shooting component to handle ALL the shooting logic for weapon
    - improved weapon animation component to handle ALL of the weapon animations logic for weapon
    - weapon now calls the shooting and animation components to do all the work

--- Players/Components/Scripts/WeaponAnimationComponent.cs
+++ Players/Components/Scripts/WeaponAnimationComponent.cs
@@ -5,54 +5,74 @@ namespace CoffeeCatProject.Players.Components.Scripts;
 // This only handles the animation logic for weapons
 public partial class WeaponAnimationComponent : Node
 {
+	public float SpriteDirection { get; set; }
 	public bool WallSlide { get; set; }
-	public bool Shooting { get; set; }
-
+	
+	private bool _shooting;
 	private bool _onCooldown;
 	
 	[Export] public Timer CooldownTimer { get; set; }
-	
-	// Animation node
-	private AnimatedSprite2D _sprite;
+	[Export] private AnimatedSprite2D Sprite { get; set; }
+	[Export] private ShootingComponent ShootingComponent  { get; set; }
 	
 	public override void _Ready()
 	{
-		// Get animation node from weapon
-		_sprite = GetParent().GetNode<AnimatedSprite2D>("sprite");
-		
 		// Set timer values
 		CooldownTimer.SetOneShot(true);
 		
 		// Connect to timer signal
 		CooldownTimer.Timeout += CooldownTimeout;
 		
-		_sprite.Play("idle");
+		// Connect to shooting component signal
+		ShootingComponent.IsSHooting += IsShooting;
+		
+		Sprite.Play("idle");
 	}
 	
 	private void CooldownTimeout()
 	{
 		_onCooldown = false;
 	}
+
+	private void IsShooting()
+	{
+		_shooting = true;
+	}
 	
 	public override async void _Process(double delta)
 	{
-		if (Shooting && !_onCooldown)
+		// Flip weapon sprite based on player's direction 
+		if (SpriteDirection < 0)
 		{
+			Sprite.FlipH = false;
+		}
+		
+		if (SpriteDirection > 0)
+		{
+			Sprite.FlipH = true;
+		}
+		
+		if (_shooting && !_onCooldown)
+		{
+			_shooting = false;
 			_onCooldown = true;
 			CooldownTimer.Start();
-			_sprite.Play("shoot");
+			Sprite.Play("shoot");
 		}
 		else if (WallSlide)
 		{
-			_sprite.Play("wall_slide");
+			_shooting = false;
+			Sprite.Play("wall_slide");
 		}
 		else
 		{
-			if (_sprite.Animation == "shoot")
+			_shooting = false;
+			
+			if (Sprite.Animation == "shoot")
 			{
-				await ToSignal(_sprite, "animation_finished");
+				await ToSignal(Sprite, "animation_finished");
 			}
-			_sprite.Play("idle");
+			Sprite.Play("idle");
 		}
 	}
 }
